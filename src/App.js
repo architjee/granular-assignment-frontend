8529666f418ff9e61a36e46836481a20443ef2fc
Archit Jain <32292295+architjee@users.noreply.github.com>
2023-01-21 19:53:11 +0530

    query working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import axios from 'axios';
 import { Component } from 'react';
 import './App.css';
 
-export default class App extends Component{
 
-  componentDidMount() {
-    console.log('hello')
+export default class App extends Component {
+  constructor(props) {
+    // Required step: always call the parent class' constructor
+    super(props);
+
+    // Set the state directly. Use props if necessary.
+    this.state = {
+      loggedIn: false,
+      currentState: "not-panic",
+
+
+      // Note: think carefully before initializing
+      // state based on props!
+      someInitialValue: this.props.initialValue
+    }
   }
-   render() {
+
+  async fetchDataFromBackend(location){
+    try {
+      const response = await axios.get('https://cors-anywhere.herokuapp.com/https://nominatim.openstreetmap.org/search.php?',{ params: { q: 'boston',format: 'jsonv2' } });
+      console.log('response from axios request',response);
+      this.setState({
+        currentState: response.data
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  componentDidMount() {
+    this.fetchDataFromBackend('boston')
+    
+
+  }
+  render() {
     return (
       <div className="App">
         Learn React
