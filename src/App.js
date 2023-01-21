fa3b3ebf7e84b007d4b0805e7114c4cbb114c803
Archit Jain <32292295+architjee@users.noreply.github.com>
2023-01-21 23:07:18 +0530

    no progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+
 import axios from 'axios';
 import { Component } from 'react';
 import './App.css';
 import SearchResults from './SearchResults';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+
 class App extends Component {
   componentDidMount() {
     this.fetchDataFromBackend(this.state.searchQuery)
@@ -52,6 +56,18 @@ class App extends Component {
         </div>
         <input type="submit" value="Submit" />
       </form>
+
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
     </div>
     );
   }
